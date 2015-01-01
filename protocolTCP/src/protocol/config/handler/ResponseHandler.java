7482ef5527eb9bdc188c5bff233b0b6f6155c381
Ovidiu Popoviciu <ovidiu.popoviciu@hotmail.co.uk>
2015-01-01 20:05:40 +0200

    modified proto files and generated classes

--- protocolTCP/src/protocol/config/handler/ResponseHandler.java
+++ protocolTCP/src/protocol/config/handler/ResponseHandler.java
@@ -1,8 +1,43 @@
 package protocol.config.handler;
 
+import protocol.config.buffers.ServerResponse.Response;
+
 public class ResponseHandler {
 
 	private ResponseHandler(){}
-	
-	
+
+	/**
+	 *
+	 * */
+	public static Response createActionResponse(boolean ok, String message){
+
+		return null;
+	}
+
+	private static Response actionResponse(){
+		return null;
+	}
+
+	/**
+	 *
+	 * */
+	public static Response createCallResponse(){
+		return null;
+	}
+
+	private static Response callResponse(){
+		return null;
+	}
+
+	/**
+	 *
+	 * */
+	public static Response createEndCallResponse(){
+		return null;
+	}
+
+	private static Response endCallResponse(){
+		return null;
+	}
+
 }
