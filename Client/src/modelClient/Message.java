0cc9e18a1affb2b35081fdf766032e44ebbf6b28
dalexro <alexander.danila@gmail.com>
2015-01-26 13:32:55 +0000

    Model Client - Minimal

---
+++ Client/src/modelClient/Message.java
@@ -0,0 +1,54 @@
+import java.io.Serializable;
+
+/**
+ * 
+ */
+public class Message implements Serializable
+{
+	private String toUser = "";
+	private String fromUser = "";
+	private String body = "";
+    private static final long serialVersionUID = 42L;
+    
+    public static final int USER = 0;
+    public static final int HASHSET = 3;
+    public static final int DC = 4;
+    public static final int REMOVED = 5;
+    public static final int CALL = 6;
+    public static final int NONEXISTANT = 41;
+    public static final int REQUEST = 48;
+    public static final int DECLINE = 7;
+    public static final int ACCEPT = 8;
+    public static final int BYE = 9;
+    public static final int SERVERDOWN = 10;
+    public static final int ERROR = 115;
+	
+	public Message()
+    {
+        this.toUser = "";
+        this.body = "";
+        this.fromUser = "";
+    }
+	
+	public Message(String fromUser,String toUser, String body)
+    {
+		this.fromUser = fromUser;
+        this.toUser = toUser;
+        this.body = body;
+    }
+	
+	public String getMessage()
+	{
+		return body;
+	}
+	
+	public String getRecipient()
+	{
+		return toUser;
+	}
+	
+	public String getOrigin()
+	{
+		return fromUser;
+	}
+}
---
+++ Client/src/modelClient/Server.java
@@ -0,0 +1,80 @@
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.io.*;
+import java.net.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+
+import javax.swing.*;
+
+/**
+ * 
+ * 
+ *
+ */
+public class Server
+{	
+    public static HashSet<User> users = new HashSet<User>();
+    public static Map<String,Socket> Maptest = new HashMap<String,Socket>();
+    public static ArrayList<String> usernames = new  ArrayList<String>();
+	public static int prevLen = 0;
+	public static int curLen = 0;
+	
+    
+	public static JFrame _serverFrame = new JFrame();
+	public static JTextArea _serverLog = new JTextArea();
+	public static JScrollPane _serverScroll = new JScrollPane();
+	
+	public static void main(String[] args)
+	{
+		int portNumber = 3000;
+	    boolean listening = true;
+	    
+	    
+	    _serverFrame.getContentPane().setLayout(null);
+	    _serverFrame.setSize(350, 200);
+	    _serverFrame.setResizable(false);
+	    _serverFrame.setTitle("ServerLogs");
+	    _serverFrame.addWindowListener(
+				new WindowAdapter()
+				{
+					public void windowClosing(WindowEvent evt) 
+					{
+						ServerThread.ServerClosed();
+					}
+				});
+	    
+	    _serverLog.setColumns(20);
+	    _serverLog.setLineWrap(true);
+	    _serverLog.setRows(5);
+	    _serverLog.setEditable(false);
+		
+	    _serverScroll.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+	    _serverScroll.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+	    _serverScroll.setViewportView(_serverLog);
+	    _serverFrame.getContentPane().add(_serverScroll);
+		_serverScroll.setBounds(10, 10 , 330, 180);
+	  
+		_serverFrame.setVisible(true);
+	    
+		try
+	    {
+	    	ServerSocket serverSocket = new ServerSocket(portNumber);
+	    	_serverLog.append("Server up!\n");
+            while (listening)
+            {
+	            new ServerThread(serverSocket.accept()).start();
+	        }
+            serverSocket.close();
+	    }
+	    catch (IOException IOerror)
+	    {
+            System.err.println("Could not start server on port: " + portNumber);
+            System.exit(-1);
+        }
+
+	}
+
+}
