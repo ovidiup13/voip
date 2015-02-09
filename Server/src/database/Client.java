d3ee200027498d979760a3160771c1310b448e87
Ovidiu Popoviciu <ovidiu.popoviciu.j@gmail.com>
2015-02-09 15:12:17 +0000

    added reference to client called

--- Server/src/database/Client.java
+++ Server/src/database/Client.java
@@ -6,26 +6,44 @@ import java.net.Socket;
  * Created by Ovidiu on 09/02/2015.
  */
 public class Client {
-    
+
     private Socket socket;
-    private int status; // 0 for IDLE, 1 for IN CALL, 2 for WAITING
+    // 0 for IDLE, 1 for IN CALL, 2 for WAITING
+    private int status; 
     private String username;
-    
-    public Client(Socket socket, int status, String username){
+    //reference to client called
+    private Client clientCalled;
+
+    public Client(Socket socket, int status, String username) {
         this.socket = socket;
         this.status = status;
         this.username = username;
+        this.clientCalled = null;
     }
-    
-    public int getStatus(){
+
+    //get status of current user
+    public int getStatus() {
         return status;
     }
-    
-    public Socket getSocket(){
+
+    //get socket of current user
+    public Socket getSocket() {
         return socket;
     }
-    
-    public void setStatus(int status){
+
+    //set status of current user
+    public void setStatus(int status) {
         this.status = status;
     }
+    
+    //set client which will be called
+    public void setClientCalled(Client client){
+        this.clientCalled = client;
+    }
+    
+    //remove the client from call
+    public void removeClientCalled(){
+        this.clientCalled = null;
+    }
+    
 }
