76a4a86770898fb0e604d4cbf0050cb78b775463
vbakayov <viktorbakayov@gmail.com>
2015-02-20 23:37:08 +0000

    add cool message notificaion pop up for the GUI  for accept/decline friend requet

--- Client/src/main/GuiMainClient.java
+++ Client/src/main/GuiMainClient.java
@@ -1,9 +1,17 @@
 package main;
 
+import java.awt.Dimension;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import java.net.URL;
 
+import javax.swing.AbstractAction;
 import javax.swing.BorderFactory;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
@@ -26,6 +34,11 @@ import javax.swing.SwingConstants;
 
 
 
+
+
+
+
+import javax.swing.WindowConstants;
 
 import tcp.sockethandler.*;
 
@@ -78,6 +91,7 @@ public class GuiMainClient {
     private static JMenuItem delfAction = new JMenuItem("Delete friend");
     private static JMenuItem helpAction = new JMenuItem("Help");
     private static JMenuItem logoutAction = new JMenuItem("Log out");
+    private static JMenuItem testNotifcaion = new JMenuItem("TestNotificaions");
  
     
 	
@@ -120,6 +134,7 @@ public class GuiMainClient {
 	
 	public static JButton startCallButton = new JButton();
 	public static  JButton stopCallButton = new JButton();
+	private static URL resource;
 	
 	
 
@@ -218,6 +233,7 @@ public class GuiMainClient {
 		optMenu.add(delfAction);
 		optMenu.add(helpAction);
 		optMenu.add(logoutAction);
+		optMenu.add(testNotifcaion);
 		
 		addfAction.addActionListener(new java.awt.event.ActionListener() {
 			public void actionPerformed(java.awt.event.ActionEvent event) {
@@ -248,6 +264,17 @@ public class GuiMainClient {
 				
 
 			}
+		});
+		
+		testNotifcaion.addActionListener(new java.awt.event.ActionListener() {
+			public void actionPerformed(java.awt.event.ActionEvent event) {
+				System.out.println("testNotificaions clicked!");
+				NotificationForSentFriendRequest("Viktor", false);
+				
+
+			}
+
+		
 		});
 		
 		mainWindow.setJMenuBar(menuBar);
@@ -618,8 +645,89 @@ public class GuiMainClient {
 	
 	
 	
+	private static void NotificationForSentFriendRequest(String username, boolean accepted) {
+		String message=null;
+		ImageIcon icon = null;
+		if(accepted){
+			 message = "User "+ username +" accepted your friend request";
+			  icon = createImageIcon("small-tick.png",
+		                "this is small tick");
+		}else{
+			 message = "User "+ username +" declined your friend request";
+			 icon = createImageIcon("small-x.png",
+		                "this is small x");
+		}
+		String header = "Message";
+		final JFrame frame = new JFrame();
+		frame.setSize(300,125);
+		frame.setLayout(new GridBagLayout());
+		GridBagConstraints constraints = new GridBagConstraints();
+		constraints.gridx = 0;
+		constraints.gridy = 0;
+		constraints.weightx = 1.0f;
+		constraints.weighty = 1.0f;
+		constraints.insets = new Insets(5, 5, 5, 5);
+		constraints.fill = GridBagConstraints.BOTH;
+		JLabel headingLabel = new JLabel(header);
+		//resource = clientThread.getContextClassLoader().getResource("small-tick.png");
+		//ImageIcon icon = new ImageIcon(resource);
+		headingLabel .setIcon(icon); 
+		headingLabel.setOpaque(false);
+		frame.add(headingLabel, constraints);
+		constraints.gridx++;
+		constraints.weightx = 0f;
+		constraints.weighty = 0f;
+		constraints.fill = GridBagConstraints.NONE;
+		constraints.anchor = GridBagConstraints.NORTH;
+		JButton closeButton = new JButton("X");
+		closeButton.setMargin(new Insets(1, 4, 1, 4));
+		closeButton.setFocusable(false);
+		frame.add(closeButton, constraints);
+		constraints.gridx = 0;
+		constraints.gridy++;
+		constraints.weightx = 1.0f;
+		constraints.weighty = 1.0f;
+		constraints.insets = new Insets(5, 5, 5, 5);
+		constraints.fill = GridBagConstraints.BOTH;
+		JLabel messageLabel = new JLabel("<Html><div style=\"text-align: center;\">"+message+"</html>");
+		frame.add(messageLabel, constraints);
+		frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+		frame.setUndecorated(true);
+		frame.setVisible(true);
+		
+		 closeButton = new JButton(new AbstractAction("x") {
+	        public void actionPerformed(final ActionEvent e) {
+	               frame.dispose();
+	        }
+	});
+		 Dimension scrSize = Toolkit.getDefaultToolkit().getScreenSize();// size of the screen
+		 Insets toolHeight = Toolkit.getDefaultToolkit().getScreenInsets(frame.getGraphicsConfiguration());// height of the task bar
+		 frame.setLocation(scrSize.width - frame.getWidth(), scrSize.height - toolHeight.bottom - frame.getHeight());
+		
+		 new Thread(){
+		      @Override
+		      public void run() {
+		           try {
+		                  Thread.sleep(5000); // time after which pop up will be disappeared.
+		                  frame.dispose();
+		           } catch (InterruptedException e) {
+		                  e.printStackTrace();
+		           }
+		      };
+		}.start();
 	
+	
+	}
 
+	protected static ImageIcon createImageIcon(String path,String description) {
+		java.net.URL imgURL = 	GuiMainClient.class.getResource(path);
+		if (imgURL != null) {
+			return new ImageIcon(imgURL, description);
+		} else {
+			System.err.println("Couldn't find file: " + path);
+			return null;
+		}
+	}
 	
 	
 	
